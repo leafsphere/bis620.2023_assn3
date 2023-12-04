bb0d3afed99a2e270e69d9a95bc7e7e09df7f1a9
leafsphere <78115182+leafsphere@users.noreply.github.com>
2023-12-04 03:38:44 -0500

    other updates

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,5 @@
-utils::globalVariables(c("X", "Y", "Z", "Acceleration"))
+utils::globalVariables(c("X", "Y", "Z", "Acceleration",
+                         "n", "newphase", "nct_id", "name",
+                         "desc", "conditionsordered", "element_text",
+                         "endpoint_met", "study_type", "removed",
+                         "Count"))
