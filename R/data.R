1526ada4f1380e303001f5792d5b5fbd8095fcfa
leafsphere <78115182+leafsphere@users.noreply.github.com>
2023-12-04 03:58:13 -0500

    documentation for data

--- R/data.R
+++ R/data.R
@@ -9,3 +9,48 @@
 #'   \item{X, Y, Z}{Accelerometry measurement (in milligravities).}
 #' }
 "accel"
+
+
+#' Conditions table from ctrialsgov duckdb with 2 columns dropped
+#'
+#' @format ## `conditions`
+#' A data frame with 815,944 rows and 2 columns:
+#' \describe{
+#'   \item{nct_id}{Unique identifier assigned to each study on clinicaltrials.gov}
+#'   \item{name}{Name of condition}
+#' }
+"conditions"
+
+
+#' Countries table from ctrialsgov duckdb with 1 column dropped
+#'
+#' @format ## `countries`
+#' A data frame with 653,881 rows and 3 columns:
+#' \describe{
+#'   \item{nct_id}{Unique identifier assigned to each study on clinicaltrials.gov}
+#'   \item{name}{Name of country}
+#'   \item{removed}{Whether country was removed or not from the study}
+#' }
+"countries"
+
+
+#' Endpoints of studies from ctrialsgov duckdb
+#'
+#' @format ## `endpoints`
+#' A data frame with 6,084 rows and 3 columns:
+#' \describe{
+#'   \item{nct_id}{Unique identifier assigned to each study on clinicaltrials.gov}
+#'   \item{endpoint_met}{Whether endpoint was met or not}
+#' }
+"endpoints"
+
+
+#' Subset of studies table from ctrialsgov duckdb
+#'
+#' @format ## `studies`
+#' A data frame with 100,000 rows and 70 columns:
+#' \describe{
+#'   \item{nct_id}{Unique identifier assigned to each study on clinicaltrials.gov}
+#'   \item{endpoint_met}{Whether endpoint was met or not}
+#' }
+"studies"
